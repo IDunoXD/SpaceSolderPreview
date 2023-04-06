c903d69b13483f72c1b3096d953d099baea2bb34
IDunoXD <114238448+IDunoXD@users.noreply.github.com>
2023-04-06 14:59:34 +0300

    002

---
+++ SpaceSolderPreview/Assets/_Game/Scripts/Joystick.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Joystick : MonoBehaviour
+{
+    [SerializeField] private float maxDragDistance = 1;
+    
+    private RectTransform _rectTransform;
+    private Vector2 _joystickDefaultPosition;
+    private Vector2 _mousePosition;
+    private Vector2 _mouseStartPosition;
+    private Vector2 _mouseDelta;
+
+    void Start()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+        _joystickDefaultPosition = _rectTransform.anchoredPosition;
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            _mouseStartPosition = Input.mousePosition;
+        }
+        if(Input.GetMouseButton(0))
+        {
+            _mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            _mouseDelta = Vector2.ClampMagnitude(_mousePosition - _mouseStartPosition, maxDragDistance);
+            _rectTransform.anchoredPosition = _joystickDefaultPosition + _mouseDelta;
+        }
+        else
+        {
+            _rectTransform.anchoredPosition = _joystickDefaultPosition;
+        }
+    }
+}
