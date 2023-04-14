5df3897020a818825aa1c0443ba29da2ea252249
IDunoXD <114238448+IDunoXD@users.noreply.github.com>
2023-04-14 20:39:10 +0300

    006

--- SpaceSolderPreview/Assets/_Game/Scripts/Shooter.cs
+++ SpaceSolderPreview/Assets/_Game/Scripts/Shooter.cs
@@ -1,47 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Shooter : MonoBehaviour
 {
+    private const float MAX_BULLET_HOLE_LIFE_TIME = 4;
+
     [SerializeField] private GameObject bulletHole;
-    [SerializeField] private Button shootButton;
+    [SerializeField] private MeshRenderer flashMesh;
     [SerializeField] private float cooldown;
     [SerializeField] private float fleshTime;
 
-    private MeshRenderer _meshRenderer;
-    private float _startShootTime;
+    private float _startShootTime = 0;
     private bool _fireing;
 	private RaycastHit _hit;
-
-	private void Start()
-    {
-        _meshRenderer = GetComponent<MeshRenderer>();
-    }
+    private bool CanShoot => _fireing && _startShootTime + cooldown < Time.time;
+    private bool HideFlashEffect => _startShootTime + fleshTime < Time.time;
 
     private void Update()
     {
-        if(_fireing)
+        if(CanShoot)
         {
             Shoot();
         }
-        if(_startShootTime + fleshTime < Time.time)
+        if(HideFlashEffect)
         {
-            _meshRenderer.enabled = false;
+            flashMesh.enabled = false;
         }
     }
 
     private void Shoot()
     {
-        if(_startShootTime + cooldown < Time.time)
+        _startShootTime = Time.time;
+        flashMesh.enabled = true;
+        if(Physics.Raycast(transform.position, transform.up, out _hit, Mathf.Infinity))
         {
-            _startShootTime = Time.time;
-            _meshRenderer.enabled = true;
-            if(Physics.Raycast(transform.position, transform.forward, out _hit, Mathf.Infinity))
-            {
-                Instantiate(bulletHole, _hit.point, Quaternion.LookRotation(_hit.normal));
-            }
+            var hole = Instantiate(bulletHole, _hit.point, Quaternion.LookRotation(_hit.normal));
+            Destroy(hole, MAX_BULLET_HOLE_LIFE_TIME);
         }
     }
 
